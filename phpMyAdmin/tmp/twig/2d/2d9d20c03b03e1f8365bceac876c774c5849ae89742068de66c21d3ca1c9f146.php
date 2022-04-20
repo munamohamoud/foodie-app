5e933240bdfa4b357262ca02c7ee547ea2e41f32
munamohamoud <munamohamoud711@gmail.com>
2022-04-20 17:41:12 -0400

    first commit

---
+++ phpMyAdmin/tmp/twig/2d/2d9d20c03b03e1f8365bceac876c774c5849ae89742068de66c21d3ca1c9f146.php
@@ -0,0 +1,67 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* message.twig */
+class __TwigTemplate_b4207a884b38b9f9378fb951a9ea92731ef4de3309e3a002c96d2e4852e92736 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div class=\"alert alert-";
+        echo twig_escape_filter($this->env, ($context["context"] ?? null), "html", null, true);
+        echo "\" role=\"alert\">
+  ";
+        // line 2
+        echo ($context["message"] ?? null);
+        echo "
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "message.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  42 => 2,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "message.twig", "/Users/munam/Downloads/munamohamoud/foodie_app/phpMyAdmin/templates/message.twig");
+    }
+}
